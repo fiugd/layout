ee7b1dcb88343b6556df23e72f485643014c9fc0
crosshj <github@chimpjuice.com>
2022-07-15 19:21:47 -0400

    tab close buttons, misc tweaks

--- src/tabbed.js
+++ src/tabbed.js
@@ -6,12 +6,20 @@ export const style = () => `
 		font-family: sans-serif; font-size: 14px;
 	}
 	.tabbedPane { border-top: 1px solid #262626; border-left: 1px solid #262626; }
-	.tabs { height: 30px; background: #333; display: flex; }
-	.tab { padding: 0.5em 1.5em; cursor: pointer; }
+	.tabs { height: 30px; background: #2a2a2a; display: flex; }
+	.tab { 
+		padding: 0.55em 1.5em; cursor: pointer; color: #999;
+		border-top-right-radius: 2px; border-top-left-radius: 2px;
+		border-left: 0.5px solid; border-right: 0.5px solid; border-top: 0.5px solid;
+		border-color: transparent;
+	}
 	.tab + .tab { margin-left: 1px; }
 	.tabbedContent { flex: 1; }
 	.tabbedContent iframe { border: 0; width: 100%; height: 100%; }
-	.active, .open, .tabbedContent iframe { background: #1e1e1e; }
+	.active, .open, .tabbedContent iframe { background: #1e1e1e; border-color: #2a2a2a; }
+	.tab-close { display: inline-block; color: transparent; }
+	.tab-close svg {  stroke: currentColor; stroke-width: 2.5; height: 0.6em; margin-left: 0.5em; margin-right: -0.3em; }
+	.active, .active .tab-close, .tab:hover .tab-close { color: white }
 `;
 
 export const createDom = (children) => `
@@ -22,7 +30,13 @@ export const createDom = (children) => `
 					class="tab${ x.active ? ' active' : ''}"
 					file="${x.iframe.split('/').pop()}"
 				>
-					${x.iframe.split('/').pop()}
+					<span>${x.iframe.split('/').pop()}</span>
+					<div class="tab-close">
+						<svg viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg">
+							<line x1="1" y1="1" x2="9" y2="9"></line>
+							<line x1="9" y1="1" x2="1" y2="9"></line>
+						</svg>
+					</div>
 				</div>
 			`).join('')}
 		</div>
